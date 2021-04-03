289ebdc641032c4cfafaeed3c1c605b399b70eb5
kalki2505 <kalkibhavsar2505@gmail.com>
2021-04-04 02:58:02 +0530

    menubar_profile_1

--- rms/src/App.js
+++ rms/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './rms_components/Login';
+import Dashboard from './rms_components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login />        
+      <Dashboard />        
     </div>
   );
 }
--- rms/src/rms_components/Login.js
+++ rms/src/rms_components/Login.js
@@ -1,5 +1,4 @@
 
-
 function Login() {
   return (
     <div className="App">
